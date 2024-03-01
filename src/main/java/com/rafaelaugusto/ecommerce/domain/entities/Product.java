75e2da5910da3b7ab2067606b32d34a71c3d2b2b
Rafael <rafaelaugustodev@gmail.com>
2024-02-29 22:26:54 -0300

    feat: creating entity Product

---
+++ src/main/java/com/rafaelaugusto/ecommerce/domain/entities/Product.java
@@ -0,0 +1,36 @@
+package com.rafaelaugusto.ecommerce.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.w3c.dom.Text;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity(name = "tb_product")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    private String imgUrl;
+
+    private Double price;
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories = new HashSet<>();
+}
