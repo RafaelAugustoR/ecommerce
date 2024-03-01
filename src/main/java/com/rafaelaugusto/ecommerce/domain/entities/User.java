1e3a6f127df8a1f2ee7c027970f68eea6a4e92f4
Rafael <rafaelaugustodev@gmail.com>
2024-02-29 22:26:14 -0300

    feat: creating entity User

---
+++ src/main/java/com/rafaelaugusto/ecommerce/domain/entities/User.java
@@ -0,0 +1,40 @@
+package com.rafaelaugusto.ecommerce.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity(name = "tb_users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, unique = true, length = 11)
+    private String phone;
+
+    @Column(nullable = false, length = 8)
+    private LocalDate birthDate;
+
+    @Column(nullable = false, length = 32)
+    private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+}
