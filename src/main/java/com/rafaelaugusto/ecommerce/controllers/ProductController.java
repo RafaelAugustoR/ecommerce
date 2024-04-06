93e5a744fbf3a6d2b710a2595e442ab847a9cebd
Rafael <rafaelaugustodev@gmail.com>
2024-04-06 12:02:40 -0300

    feat: validating requests for productById

--- src/main/java/com/rafaelaugusto/ecommerce/controllers/ProductController.java
+++ src/main/java/com/rafaelaugusto/ecommerce/controllers/ProductController.java
@@ -2,12 +2,16 @@ package com.rafaelaugusto.ecommerce.controllers;
 
 import com.rafaelaugusto.ecommerce.dtos.response.ProductResponseDTO;
 import com.rafaelaugusto.ecommerce.services.ProductService;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequestMapping("/api")
 public class ProductController {
@@ -16,7 +20,7 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products/{id}")
-    public ProductResponseDTO productById(@PathVariable Long id){
+    public ProductResponseDTO productById(@PathVariable @NotNull @Positive Long id) {
         return productService.findById(id);
     }
 }
