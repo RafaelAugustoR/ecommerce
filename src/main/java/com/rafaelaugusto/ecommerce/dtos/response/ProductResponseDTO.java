3ff527193a648fc12bb65f0615905adb9566140b
Rafael <rafaelaugustodev@gmail.com>
2024-04-06 12:07:25 -0300

    fix: corrected error where properties were not copied to the ProductResponseDTO

--- src/main/java/com/rafaelaugusto/ecommerce/dtos/response/ProductResponseDTO.java
+++ src/main/java/com/rafaelaugusto/ecommerce/dtos/response/ProductResponseDTO.java
@@ -1,9 +1,11 @@
 package com.rafaelaugusto.ecommerce.dtos.response;
 
+import com.rafaelaugusto.ecommerce.domain.entities.Product;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.BeanUtils;
 
 @Data
 @AllArgsConstructor
@@ -18,4 +20,8 @@ public class ProductResponseDTO {
     private String imgUrl;
 
     private Double price;
+
+    public ProductResponseDTO(Product entity) {
+        BeanUtils.copyProperties(entity, this);
+    }
 }
