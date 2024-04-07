dbb6708f1146cf86771167f5eed7e7b2996328a7
Rafael <rafaelaugustodev@gmail.com>
2024-04-06 21:11:56 -0300

    refactor: changing endpoints

--- src/main/java/com/rafaelaugusto/ecommerce/controllers/ProductController.java
+++ src/main/java/com/rafaelaugusto/ecommerce/controllers/ProductController.java
@@ -8,25 +8,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Validated
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/products")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/products/{id}")
+    @GetMapping("/{id}")
     public ProductResponseDTO productById(@PathVariable @NotNull @Positive Long id) {
         return productService.findById(id);
     }
 
-    @GetMapping("/products")
+    @GetMapping
     public Page<ProductResponseDTO> allProducts(Pageable pageable) {
         return productService.findAll(pageable);
     }
