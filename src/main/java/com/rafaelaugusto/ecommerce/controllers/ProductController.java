7b9dde34ec975a0fdb5695809c09ae22e0ae56f8
Rafael <rafaelaugustodev@gmail.com>
2024-04-06 22:42:16 -0300

    feat: adding endpoint to receive requests for creating new products

--- src/main/java/com/rafaelaugusto/ecommerce/controllers/ProductController.java
+++ src/main/java/com/rafaelaugusto/ecommerce/controllers/ProductController.java
@@ -1,12 +1,16 @@
 package com.rafaelaugusto.ecommerce.controllers;
 
+import com.rafaelaugusto.ecommerce.dtos.request.ProductRequestDTO;
 import com.rafaelaugusto.ecommerce.dtos.response.ProductResponseDTO;
 import com.rafaelaugusto.ecommerce.services.ProductService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,13 +22,22 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @PostMapping("/create")
+    public ResponseEntity<ProductResponseDTO> createProduct(@Valid @RequestBody ProductRequestDTO request) {
+        var createdProduct = productService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
+    }
+
+
     @GetMapping("/{id}")
-    public ProductResponseDTO productById(@PathVariable @NotNull @Positive Long id) {
-        return productService.findById(id);
+    public ResponseEntity<ProductResponseDTO> productById(@PathVariable @NotNull @Positive Long id) {
+        var foundProduct = productService.findById(id);
+        return ResponseEntity.ok(foundProduct);
     }
 
     @GetMapping
-    public Page<ProductResponseDTO> allProducts(Pageable pageable) {
-        return productService.findAll(pageable);
+    public ResponseEntity<Page<ProductResponseDTO>> allProducts(Pageable pageable) {
+        var products = productService.findAll(pageable);
+        return ResponseEntity.ok(products);
     }
 }
